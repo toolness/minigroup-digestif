0b1481dad1866fc5f18aeb5ffdc9383857ec5683
Atul Varma <varmaa@gmail.com>
2013-11-21 14:31:18 -0500

    parse subject in minigroup notifications.

--- process-minigroup-post.js
+++ process-minigroup-post.js
@@ -1,14 +1,21 @@
 var cheerio = require('cheerio');
 
+var SUBJECT_MATCH = /^(?:Re\: )?(.*) — (.+)$/;
+
 function processMinigroupPost(mail) {
   if (mail.from[0].address == 'notification@minigroupmail.com') {
     var $ = cheerio.load(mail.html);
+    var subjectMatch = mail.subject.match(SUBJECT_MATCH);
 
     mail.minigroup = {
       from: mail.from[0].name.split(' via Minigroup')[0],
       thumbnailURL: $('img.thumb').attr('src'),
       html: $('div.formatted').html()
     };
+    if (subjectMatch) {
+      mail.minigroup.subject = subjectMatch[1];
+      mail.minigroup.group = subjectMatch[2];
+    }
     $('a[href^="http://email.minigroupmail.com"]').each(function() {
       if ($(this).text().match(/View this post/i))
         mail.minigroup.viewURL = $(this).attr("href");
